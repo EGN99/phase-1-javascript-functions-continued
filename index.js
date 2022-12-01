f14a9aef9c94a6de730af69a7bb8ea93c1dffa1f
Elvis Njoroge Gitau <elvis.gitau@student.moringaschool.com>
2022-12-01 16:05:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+function saturdayFun (activity){
+    if (activity===undefined) {
+        console.log(`This Saturday, I Will roller-skate!`)
+    }else {
+        console.log(`This Saturday, I Will ${activity}!`)
+    }
+ }
+saturdayFun("bathe my dog");
+
+
+function mondayWork (activity) {
+    if (activity===undefined) {
+        let activity = "go to the office"
+    console.log (`This Monday, I will ${activity}.`);
+}else {
+    console.log (`This Monday, I will ${activity}.`);
+}
+}
+mondayWork("work from home");
+
+function wrapAdjective(whatever = "*"){
+    return function(adjective = "special"){
+        return `You are ${whatever}${adjective}${whatever}!`
+    }
+}
